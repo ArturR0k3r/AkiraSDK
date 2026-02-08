efa8129702c541e8bb00925e8d9244c29d7ab468
ArturR0k3r <artur2003s.ass@gmail.com>
2026-02-08 03:58:37 +0200

    migration of AkiraSDK for the v1.4.x

--- wasm_apps/hello_world/main.c
+++ wasm_apps/hello_world/main.c
@@ -2,46 +2,21 @@
  * @copyright Copyright © contributors to Project Ocre,
  * which has been established as Project Ocre a Series of LF Projects, LLC
  * SPDX-License-Identifier: Apache-2.0
- * 
- * AkiraOS Hello World WASM App - minimal no-stdlib version
- * 
- * Compiles with -nostdlib to avoid WASI imports
- * Calls putchar directly from env module (provided by WAMR libc-builtin)
+ *
+ * AkiraOS Hello World WASM App
  */
 
-/* Declare external functions from env module (WAMR libc-builtin) */
-extern int putchar(int c);
+int log(int level, const char *message);
+
+/* Log levels */
+#define LOG_ERROR 0
+#define LOG_WARN 1
+#define LOG_INFO 2
+#define LOG_DEBUG 3
 
 int main(void)
 {
-    putchar('H');
-    putchar('e');
-    putchar('l');
-    putchar('l');
-    putchar('o');
-    putchar(' ');
-    putchar('f');
-    putchar('r');
-    putchar('o');
-    putchar('m');
-    putchar(' ');
-    putchar('W');
-    putchar('A');
-    putchar('S');
-    putchar('M');
-    putchar(' ');
-    putchar('o');
-    putchar('n');
-    putchar(' ');
-    putchar('A');
-    putchar('k');
-    putchar('i');
-    putchar('r');
-    putchar('a');
-    putchar('O');
-    putchar('S');
-    putchar('!');
-    putchar('\n');
-    
+    log(LOG_INFO, "Hello, World from AkiraOS WASM App!");
+
     return 0;
 }
